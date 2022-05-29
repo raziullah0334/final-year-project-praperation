90026a093d3720b7c16eb4ab64d974f8eb505f2b
Raziullah <raziullahkhanbakakhel@gmail.com>
2022-05-29 05:57:40 +0500

    added to app.js

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Home from "./Home"
+
+
+
+function  Home(props) {
+  return (
+    <div>
+        <div>
+          
+          <input type="text" placeholder="phone or email" className='phone-tag'/> 
+          <input type="password" placeholder="password" className="phone-tag" id='password'/>
+        <button className='btn btn-primary'>larg button</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home;
